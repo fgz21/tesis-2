2671affaabeae74df315e836650f35f7bd9871ce
Erick Ramirez <erickgiber7@gmail.com>
2023-06-22 23:45:05 -0400

    New improvements

--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,4 @@
-const router = new Navigo('/', { hash: true });
+const router = new Navigo("/", { hash: true });
 
 // Esta función es para extraer el html de otro archivo
 async function getHtmlAsString(filePath) {
@@ -11,20 +11,28 @@ async function getHtmlAsString(filePath) {
   }
 }
 
-router.on('/dashboard', async () => {
-    /* Aqui se debe comprobar que esta inciado sesion */
-    // Cambia el valor a true para ver el contenido
-    const isAuth = false;
+router.on("/dashboard", async () => {
+  /* Aqui se debe comprobar que esta inciado sesion */
+  // Cambia el valor a true para ver el contenido
+  const isAuth = false;
 
-    // Si isAuth es falso no entrar al contenido
-    if (!isAuth) {
-      return router.navigate('/');
-    }
+  // Si isAuth es falso no entrar al contenido
+  if (!isAuth) {
+    return router.navigate("/");
+  }
 
-    // De lo contrario el carga el contenido
-    const htmlString = await getHtmlAsString('/pages/dashboard.html');
-    document.body.innerHTML = htmlString;
-    console.info(htmlString);
-  });
+  // De lo contrario el carga el contenido
+  const htmlString = await getHtmlAsString("/pages/dashboard.html");
+  document.body.innerHTML = htmlString;
+});
 
+//* Para añadir mas rutas es así:
+/*
+router.on('/ruta', () => {
+  const htmlString = await getHtmlAsString("/la-ruta-del-html");
+  document.body.innerHTML = htmlString;
+})
+*/
+
+// Esto siempre queda al final para que funcione las rutas
 router.resolve();
